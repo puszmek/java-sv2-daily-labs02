482c10792284fd94a83a7976bb2bb35d545b7d29
he <bgyhenrietta@yahoo.com>
2021-10-18 19:05:28 +0200

    week02 day01 Human

---
+++ week02-labs-tasks/src/main/java/day01/HumanMain.java
@@ -0,0 +1,14 @@
+package day01;
+
+public class HumanMain {
+
+    public static void main(String[] args) {
+
+        Human human = new Human("Bob", 22);
+
+        System.out.println("Name: " + human.getName());
+        System.out.println("Age: " + human.getAge());
+
+    }
+
+}
